f559040dcf1eb0cd46a33ff44245b005e4c0761f
Tanvir Ahamed Sowmik <111014007+sowmik265@users.noreply.github.com>
2023-06-15 00:36:41 +0600

    login & reg page design change

--- src/SignIn&SignUp/SignUp.jsx
+++ src/SignIn&SignUp/SignUp.jsx
@@ -50,12 +50,12 @@ const SignUp = () => {
 
     return (
         <>
-        <Helmet>
-            <title> Melody Lab | Register</title>
-        </Helmet>
+            <Helmet>
+                <title> Melody Lab | Register</title>
+            </Helmet>
             <div className="relative flex flex-col justify-center min-h-screen overflow-hidden mx-2">
                 <div className="w-full p-6 m-auto rounded-md shadow-xl lg:max-w-xl bg-rose-700 mt-6 mx-auto">
-                    <h1 className="text-3xl font-semibold text-center text-gray-200 uppercase">
+                    <h1 className="text-3xl font-semibold text-center text-yellow-300 uppercase">
                         Register
                     </h1>
 
@@ -63,14 +63,14 @@ const SignUp = () => {
                         <div className="mb-2">
                             <label
                                 for="name"
-                                className="block text-sm font-semibold text-gray-200"
+                                className="block text-sm font-semibold text-yellow-300"
                             >
                                 Name
                             </label>
                             <input
                                 type="text"
                                 placeholder='Your name'
-                                className="block w-full px-4 py-2 mt-2 text-rose-600 bg-white border rounded-md focus:border-rose-400 focus:ring-rose-200 focus:outline-none focus:ring focus:ring-opacity-40"
+                                className="block w-full px-4 py-2 mt-2 text-rose-600 bg-white border rounded-md focus:border-yellow-400 focus:ring-yellow-300 focus:outline-none focus:ring focus:ring-opacity-40"
                                 {...register("name", { required: true, maxLength: 20 })}
                             />
                             {errors.name && <span className='text-yellow-300'>This field is required</span>}
@@ -79,14 +79,14 @@ const SignUp = () => {
                         <div className="mb-2">
                             <label
                                 for="photo"
-                                className="block text-sm font-semibold text-gray-200"
+                                className="block text-sm font-semibold text-yellow-300"
                             >
                                 Photo URL
                             </label>
                             <input
                                 type="text"
                                 placeholder='Your photo URL'
-                                className="block w-full px-4 py-2 mt-2 text-rose-600 bg-white border rounded-md focus:border-rose-400 focus:ring-rose-200 focus:outline-none focus:ring focus:ring-opacity-40"
+                                className="block w-full px-4 py-2 mt-2 text-rose-600 bg-white border rounded-md focus:border-yellow-400 focus:ring-yellow-300 focus:outline-none focus:ring focus:ring-opacity-40"
                                 {...register("photo", { required: true })}
                             />
                             {errors.photo && <span className='text-yellow-300'>This field is required</span>}
@@ -94,14 +94,14 @@ const SignUp = () => {
                         <div className="mb-2">
                             <label
                                 for="email"
-                                className="block text-sm font-semibold text-gray-200"
+                                className="block text-sm font-semibol text-yellow-300"
                             >
                                 Email
                             </label>
                             <input
                                 type="email"
                                 placeholder='Your email address'
-                                className="block w-full px-4 py-2 mt-2 text-rose-600 bg-white border rounded-md focus:border-rose-400 focus:ring-rose-200 focus:outline-none focus:ring focus:ring-opacity-40"
+                                className="block w-full px-4 py-2 mt-2 text-rose-600 bg-white border rounded-md focus:border-yellow-400 focus:ring-yellow-300 focus:outline-none focus:ring focus:ring-opacity-40"
                                 {...register("email", { required: true })}
                             />
                             {errors.email && <span className='text-yellow-300'>This field is required</span>}
@@ -109,14 +109,14 @@ const SignUp = () => {
                         <div className="mb-2">
                             <label
                                 for="password"
-                                className="block text-sm font-semibold text-gray-200"
+                                className="block text-sm font-semibold text-yellow-300"
                             >
                                 Password
                             </label>
                             <input
                                 type="password"
                                 placeholder='Your password'
-                                className="block w-full px-4 py-2 mt-2 text-rose-600 bg-white border rounded-md focus:border-rose-400 focus:ring-rose-200 focus:outline-none focus:ring focus:ring-opacity-40"
+                                className="block w-full px-4 py-2 mt-2 text-rose-600 bg-white border rounded-md focus:border-yellow-400 focus:ring-yellow-300 focus:outline-none focus:ring focus:ring-opacity-40"
                                 {...register("password", {
                                     required: true,
                                     minLength: 6,
@@ -130,7 +130,7 @@ const SignUp = () => {
                             {errors.password?.type === 'pattern' && <p className="text-yellow-300">Password must have one Uppercase one lower case, one number and one special character.</p>}
                         </div>
                         <div className="mt-6">
-                            <button className="w-full px-4 py-2 tracking-wide text-white transition-colors duration-200 transform bg-rose-300 rounded-md hover:bg-rose-200 focus:outline-none focus:bg-rose-600">
+                            <button className="btn btn-outline btn-warning border-b-4 w-full px-4 py-2 tracking-wide text-white transition-colors duration-200 transform bg-rose-600 rounded-md hover:bg-yellow-300 focus:outline-none focus:bg-rose-600">
                                 Register
                             </button>
                         </div>
