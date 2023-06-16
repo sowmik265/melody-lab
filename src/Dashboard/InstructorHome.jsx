799bc9be525c3c1e895caa0cdb682277e6bcd567
Tanvir Ahamed Sowmik <111014007+sowmik265@users.noreply.github.com>
2023-06-16 09:26:46 +0600

    inst dash done

--- src/Dashboard/InstructorHome.jsx
+++ src/Dashboard/InstructorHome.jsx
@@ -2,9 +2,12 @@ import React from 'react';
 import { Helmet } from 'react-helmet-async';
 import useAuth from '../hooks/useAuth';
 import { AuthContext } from '../Providers/AuthProvider';
+import useClass from '../hooks/useClass';
 
 const InstructorHome = () => {
     const {user} = useAuth(AuthContext);
+    const [classes] = useClass();
+    const myClass = classes.filter((my) => my.email === `${user?.email}`);
     return (
         <div>
             <Helmet>
@@ -19,14 +22,14 @@ const InstructorHome = () => {
 
                 <div className="w-full md:w-60 p-2 rounded-xl transform transition-all hover:-translate-y-2 duration-300 shadow-lg hover:shadow-2xl m-2 bg-yellow-500">
                     <div className="p-2 flex flex-col gap-2 text-center">
-                        <h2 className="font-bold text-7xl mb-2 text-rose-700"></h2>
+                        <h2 className="font-bold text-7xl mb-2 text-rose-700">{myClass.length}</h2>
                         <p className="font-bold text-lg text-rose-700">Approved Classes</p>
                     </div>
                 </div>
 
                 <div className="w-full md:w-60 p-2 rounded-xl transform transition-all hover:-translate-y-2 duration-300 shadow-lg hover:shadow-2xl m-2 bg-blue-500">
                     <div className="p-2 flex flex-col gap-2 text-center">
-                        <h2 className="font-bold text-7xl mb-2 text-rose-700"></h2>
+                        <h2 className="font-bold text-7xl mb-2 text-rose-700">5</h2>
                         <p className="font-bold text-lg text-rose-700">Students</p>
                     </div>
                 </div>
--- src/Dashboard/Myclass.jsx
+++ src/Dashboard/Myclass.jsx
@@ -1,10 +1,78 @@
 import React from 'react';
+import { useQuery } from "@tanstack/react-query";
+import useAuth from "../hooks/useAuth";
+import useAxiosSecure from "../hooks/useAxiosSecure";
+import useClass from '../hooks/useClass';
+import SectionTile from '../Utilitites/SectionTile';
+
+
+
+
+
 
 const Myclass = () => {
+    const { user, loading } = useAuth();
+  
+
+    const [classes] = useClass();
+    const myClass = classes.filter((my) => my.email === `${user?.email}`);
+    // console.log(myClass)
+
     return (
-        <div>
-            sdgdsgsgsgs
-        </div>
+
+        <>
+
+        <SectionTile title={'My All classes'}></SectionTile>
+
+            <div className="overflow-x-auto w-full">
+                <table className="table w-full">
+                    {/* head */}
+                    <thead>
+                        <tr>
+                            <th>#</th>
+                            <th>Item</th>
+                            <th>Available Seats</th>
+                            <th>Price</th>
+                            <th>Schedule</th>
+                            <th>Status</th>
+                        </tr>
+                    </thead>
+                    <tbody>
+                        {
+                            myClass.map((item, index) => <tr key={item._id}>
+                                <td>
+                                    {index + 1}
+                                </td>
+                                <td>
+                                    <div className="flex items-center space-x-3">
+                                        <div className="avatar">
+                                            <div className="mask mask-squircle w-12 h-12">
+                                                <img src={item.classImage} alt="Avatar Tailwind CSS Component" />
+                                            </div>
+                                        </div>
+                                        <div>
+                                            <div className="font-bold">{item.className}</div>
+                                        </div>
+                                    </div>
+                                </td>
+                                <td>
+                                    {item.totalSeats}
+                                </td>
+                                <td>${item.price}</td>
+                                <td>{item.time}</td>
+                                <td>
+                                    {item.status === 'approved' ? <p className=' text-green-600 font-bold'>Approved</p> : <p className=' text-rose-600 font-bold'>waiting for approval</p>}
+                                </td>
+                            </tr>)
+                        }
+
+                    </tbody>
+                </table>
+            </div>
+        </>
+
+
+
     );
 };
 
